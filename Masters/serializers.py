277f21d722dd54739b6bfeb81b83ac1cb1993258
imran76327 <is371030@gmail.com>
2024-10-22 11:04:58 +0530

    changes

--- Masters/serializers.py
+++ Masters/serializers.py
@@ -9,16 +9,22 @@ class CompanyMasterSerializer(serializers.ModelSerializer):
         fields = '__all__'  # Include all fields, or specify specific fields as needed
 
 
+class ParameterMasterSerializer(serializers.ModelSerializer):  # Corrected serializer class name
+    class Meta:
+        model = parameter_master  # Assuming the model name is in PascalCase, adjust if necessary
+        fields = '__all__'  # This will include all fields in the serialization
+
 
 class ScRosterSerializer(serializers.ModelSerializer):
     company = CompanyMasterSerializer()  # Use the nested serializer
 
     class Meta:
         model = sc_roster
-        fields = '__all__'  # Include all fields, or specify specific fields as needed
+        fields = '__all__'  
 
 class EmployeeSerializer(serializers.ModelSerializer):
     company_id = CompanyMasterSerializer()
+    employement_status_id = ParameterMasterSerializer()
     class Meta:
         model = sc_employee_master
-        fields = '__all__'  # Include all fields, or specify specific fields as needed
+        fields = '__all__' 
