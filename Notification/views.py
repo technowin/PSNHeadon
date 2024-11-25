2c416b60e2cfe49461fba9a8d3bf15e833e3b8a4
imran76327 <is371030@gmail.com>
2024-11-25 18:07:46 +0530

    sdfs

--- Notification/views.py
+++ Notification/views.py
@@ -27,11 +27,60 @@ from google.oauth2 import service_account
 from google.auth.transport.requests import Request
 from rest_framework import status
 
-from Masters.serializers import ScRosterSerializer, SlotDetailsSerializer, UserSlotDetailsSerializer, UserSlotDetailsSerializer1, UserSlotlistSerializer
+from Masters.serializers import SlotDetailsSerializer, UserSlotDetailsSerializer1, UserSlotlistSerializer
 from Notification.models import notification_log, user_notification_log
 from datetime import datetime, timedelta
 
 from Payroll.models import slot_attendance_details
+
+from django.shortcuts import get_object_or_404, render
+from django.http import JsonResponse
+from django.utils.timezone import now
+from datetime import timedelta
+
+from Masters.models import *
+from celery import shared_task
+from django.utils import timezone
+from datetime import timedelta
+from rest_framework.response import Response
+import requests
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+# Create your views here.
+# def check_shift_for_next_day(request, employee_id, site_id):
+#     tomorrow = now().date() + timedelta(days=1)
+    
+#     try:
+#         site = site_master.objects.get(site_id=site_id)
+        
+#         shift = sc_roster.objects.get(employee_id=employee_id, site=site, shift_date=tomorrow)
+        
+#         response_data = {
+#             'has_shift': True,
+#             'notification_time': site.notification_time.strftime('%H:%M'),
+#             'shift_from': shift.shift_from.strftime('%H:%M'),
+#             'shift_to': shift.shift_to.strftime('%H:%M'),
+#         }
+#     except sc_roster.DoesNotExist:
+#         response_data = {'has_shift': False}
+    
+#     return JsonResponse(response_data)
+import json
+import google.auth
+from google.oauth2 import service_account
+import google.auth.transport.requests
+import os
+
+import base64
+import json
+from google.oauth2 import service_account
+from google.auth.transport.requests import Request
+from rest_framework import status
+
+from Notification.models import notification_log
+from datetime import datetime, timedelta
+
 # Create your views here.
 # def check_shift_for_next_day(request, employee_id, site_id):
 #     tomorrow = now().date() + timedelta(days=1)
@@ -527,17 +576,102 @@ class check_and_notify_default_users(APIView):
             
                     
 
+# def send_push_notification(user,shift_data,notification_log_id):
+#     try:
+        
+#         # Retrieve and decode the base64-encoded credentials
+#         credentials_base64 = os.getenv('GOOGLE_APPLICATION_CREDENTIALS_BASE64')
+#         if not credentials_base64:
+#             raise ValueError("GOOGLE_APPLICATION_CREDENTIALS_BASE64 environment variable is not set")
+
+#         credentials_json = base64.b64decode(credentials_base64).decode('utf-8')
+#         credentials_dict = json.loads(credentials_json)
+# # Check if the decoded credentials match the original JSON file
+
+#         # Create credentials object
+#         SCOPES = ['https://www.googleapis.com/auth/firebase.messaging']
+        
+#         credentials = service_account.Credentials.from_service_account_info(credentials_dict,scopes=SCOPES)
+
+#         credentials_json = base64.b64decode(os.environ["GOOGLE_APPLICATION_CREDENTIALS_BASE64"]).decode("utf-8")
+#         credentials = json.loads(credentials_json)
+
+#         # Ensure the JSON structure is intact
+#         print(credentials)
+
+#         # Now use credentials as needed
+#         request = Request()
+#         credentials.refresh(request)
+#         access_token = credentials.token
+#         user_device_token = user.device_token
+
+#         print("Credentials have been set up successfully.")
+
+    
+#         # SERVICE_ACCOUNT_FILE="./service-account-file.json"
+#         # SCOPES = ['https://www.googleapis.com/auth/firebase.messaging']
+        
+#         # credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE,scopes=SCOPES)
+#         # request = google.auth.transport.requests.Request()
+#         # credentials.refresh(request)
+#         # access_token = credentials.token
+#         # # The Firebase device token for the user. This should be saved in your user model or a related model.
+#         # user_device_token = user.device_token  # Assuming you have this field
+#         if not user_device_token:
+#             print("No device token found for user.")
+#             return  f"error sending no device for user"
+#         serialized_shift_data = json.dumps(shift_data)
+#         shift_date = shift_data.get('shift_date')
+#         # Construct the notification payload
+#         payload = {
+#             "message":{
+#                 'token': user_device_token,
+#                 'notification': {
+#                     'title': 'Upcoming Shift Reminder',
+#                     'body': 'You have a shift scheduled for '+ shift_date,
+#                     # "click_action": "FLUTTER_NOTIFICATION_CLICK"
+#                     # 'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+#                     # 'sound': 'default'
+#                 },
+#                 'data': {
+#                     'title': 'Upcoming Shift Reminder',
+#                     'body': 'You have a shift scheduled for '+ shift_date,
+#                     'type': 'shift_reminder',
+#                     'shift_data':serialized_shift_data,
+#                     'notification_log_id':str(notification_log_id),
+#                 }
+                
+#             }
+#         }
+#         headers = {
+#             'Authorization': f'Bearer {access_token}',
+#             'Content-Type': 'application/json'
+#         }
+#         # Send the request to FCM
+#         response = requests.post('https://fcm.googleapis.com/v1/projects/appnotification-85128/messages:send', json=payload, headers=headers)
+#         if response.status_code == 200:
+#             print("Notification sent successfully.")
+#             return "success"
+#         else:
+#             response_data = json.loads(response.text)
+#             message = response_data.get('error', {}).get('message', '')
+#             print(f"Failed to send notification. Status Code: {response.status_code}, Response: {message}")
+#             return f"error sending {response.status_code}--{message}"
+            
+#     except Exception as e:
+#         print(str(e))
+#         return f"error sending {str(e)}--{str(e)}"
+
 def send_push_notification(user,shift_data,notification_log_id):
     try:
         
         # Retrieve and decode the base64-encoded credentials
-        credentials_base64 = os.getenv('GOOGLE_APPLICATION_CREDENTIALS_BASE64')
+        credentials_base64 = os.getenv('GOOGLE_APPLICATION_CREDENTIALS_BASE60')
         if not credentials_base64:
             raise ValueError("GOOGLE_APPLICATION_CREDENTIALS_BASE64 environment variable is not set")
 
         credentials_json = base64.b64decode(credentials_base64).decode('utf-8')
         credentials_dict = json.loads(credentials_json)
-# Check if the decoded credentials match the original JSON file
 
         # Create credentials object
         SCOPES = ['https://www.googleapis.com/auth/firebase.messaging']
@@ -564,21 +698,20 @@ def send_push_notification(user,shift_data,notification_log_id):
             print("No device token found for user.")
             return  f"error sending no device for user"
         serialized_shift_data = json.dumps(shift_data)
-        shift_date = shift_data.get('shift_date')
         # Construct the notification payload
         payload = {
             "message":{
                 'token': user_device_token,
                 'notification': {
                     'title': 'Upcoming Shift Reminder',
-                    'body': 'You have a shift scheduled for '+ shift_date,
+                    'body': 'You have a shift scheduled for tomorrow.',
                     # "click_action": "FLUTTER_NOTIFICATION_CLICK"
                     # 'click_action': 'FLUTTER_NOTIFICATION_CLICK',
                     # 'sound': 'default'
                 },
                 'data': {
                     'title': 'Upcoming Shift Reminder',
-                    'body': 'You have a shift scheduled for '+ shift_date,
+                    'body': 'You have a shift scheduled for tomorrow.',
                     'type': 'shift_reminder',
                     'shift_data':serialized_shift_data,
                     'notification_log_id':str(notification_log_id),
